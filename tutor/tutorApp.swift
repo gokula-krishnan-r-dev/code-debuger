583e639ac4e39999de7b6b52b977be328ec72682
gokula <gokulakrishnanr812@gmail.com>
2025-03-20 20:00:38 +0530

    Initial Commit

---
+++ tutor/tutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorApp.swift
+//  tutor
+//
+//  Created by gokul on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct tutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tutorTests/tutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  tutorTests.swift
+//  tutorTests
+//
+//  Created by gokul on 20/03/25.
+//
+
+import Testing
+@testable import tutor
+
+struct tutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
